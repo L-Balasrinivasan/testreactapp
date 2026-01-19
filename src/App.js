01d7228eb6ef25c7a5dc898c82d79713842070ee
Bala L <emindsdevops@gmail.com>
2026-01-19 23:08:16 +0530

    build added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value} this is from the bala build </div>;
 }
 
 export default App;
